e49ead56f665c032cffa1251c1490bc107a4d66c
Jon Maldia <jon@maldia.net>
2015-08-15 23:32:16 -0500

    Added navigation bar and segue
    Added navigation to views. Created a segue and code to pass data
    through the segue.

--- AlgoRhythm/PlaylistDetailViewController.swift
+++ AlgoRhythm/PlaylistDetailViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class PlaylistDetailViewController: UIViewController {
 
+    @IBOutlet weak var buttonPressedLabel: UILabel!
+    var segueLabelText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        buttonPressedLabel.text = segueLabelText
     }
 
     override func didReceiveMemoryWarning() {
