2e167a3df5e7171af2bbcb0eeeb7dd2dd569ae78
Jon Maldia <jon@maldia.net>
2015-08-16 22:44:59 -0500

    Adding more stuff
    Main additions are 2 new files for the data model

---
+++ AlgoRhythm/Playlist.swift
@@ -0,0 +1,21 @@
+//
+//  Playlist.swift
+//  AlgoRhythm
+//
+//  Created by Jon Maldia on 8/16/15.
+//  Copyright (c) 2015 Jon Maldia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+}
