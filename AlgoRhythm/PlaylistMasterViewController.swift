d2000c10732807b9d419a4a9b9a2180302b467d0
Jon Maldia <jon@maldia.net>
2015-08-26 22:51:31 -0500

    Added code for touch
    Added detail code to display detail.
    Added touch functionality to image on master

--- AlgoRhythm/PlaylistMasterViewController.swift
+++ AlgoRhythm/PlaylistMasterViewController.swift
@@ -10,13 +10,23 @@
 import UIKit
 
 class PlaylistMasterViewController: UIViewController {
-
-    @IBOutlet weak var aButton: UIButton!
+    
+    var playlistsArray: [UIImageView] = []
+    @IBOutlet weak var playlistImageView0: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        aButton.setTitle("Press me!", forState: .Normal)
+        playlistsArray.append(playlistImageView0)
+        
+        for index in 0..<playlistsArray.count {
+            let playlist = Playlist(index: index)
+            let playlistImageView = playlistsArray[index]
+            
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +35,15 @@ class PlaylistMasterViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showPlaylistDetail" {
+        if segue.identifier == "showPlaylistDetailSegue" {
             let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
-            playlistDetailController.segueLabelText = "Yay! You pressed the button!"
+        playlistDetailController.playlist = Playlist(index: 0)
         }
     }
+
+    @IBAction func showPlaylistDetail(sender: AnyObject) {
+        performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
+    }
+    
 }
 
